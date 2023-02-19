451bc1031bdba708a0fbad7e4ced576afc267dcc
Kaneis Zontanos <ramim@bc.edu>
2023-02-19 15:46:05 -0500

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Kaneis Zontanos on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
