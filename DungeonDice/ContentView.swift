d6629640968beba6e72441428e4f3e0c1f3a47cc
Kaneis Zontanos <ramim@bc.edu>
2023-02-19 16:53:14 -0500

    committing 4.4

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -42,7 +42,7 @@ struct ContentView: View {
             
             Spacer()
         
-            Group{
+            LazyVGrid (columns: [GridItem(.adaptive(minimum: 102))]){
                 ForEach(Dice.allCases, id: \.self) { dice in
                     Button("\(dice.rawValue)-sided") {
                         resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
